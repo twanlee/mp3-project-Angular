d58a762fb5d05de1d19f9c104452f67907ab8c49
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-08-10 18:37:33 +0700

    lưu trữ jwt token vào localStorage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+// @ts-ignore
 import { NgModule } from '@angular/core';
+// @ts-ignore
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
+// @ts-ignore
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
