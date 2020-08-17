5b199b4cb6db9a1d62795299a98fc6112f3b33f8
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-08-17 15:49:59 +0700

    thêm thanh phát media vào footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {StorageService} from '../../services/storage.service';
+
+declare var $: any;
+declare var APlayer: any;
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  constructor(private storageService: StorageService) {
+  }
 
   ngOnInit(): void {
+    let library: any;
+    let ap = new APlayer({
+      container: document.getElementById('aplayer'),
+      listFoled: true,
+      fixed: true,
+      audio: []
+    });
+    this.storageService.watchStorage().subscribe(data => {
+        library = JSON.parse(sessionStorage.getItem('library'));
+        ap.list.add(library[0]);
+        console.log(ap.audio);
+    });
+
   }
 
 }
