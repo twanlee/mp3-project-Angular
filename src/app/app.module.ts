c0f1ee39e9b8cbbd4b93113ebf20cb35f94cb62d
twanlee <levantuan6898@gmail.com>
2020-08-31 16:29:23 +0700

    test 123 chạy thử

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './components/user/register/register.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RegisterComponent} from './components/user/register/register.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {HeaderComponent} from './components/header/header.component';
@@ -21,45 +21,44 @@ import {EditProfileComponent} from './components/user/edit-profile/edit-profile.
 import {NgxPaginationModule} from 'ngx-pagination';
 import {authInterceptorProviders} from './helper/auth.interceptor';
 import {APP_BASE_HREF} from '@angular/common';
-import { GreatestSongComponent } from './components/songs/greatest-song/greatest-song.component';
-import { Top10songComponent } from './components/songs/top10song/top10song.component';
-import { SongPlayerComponent } from './components/songs/song-player/song-player.component';
-import { SongDescriptionComponent } from './components/songs/song-description/song-description.component';
+import {GreatestSongComponent} from './components/songs/greatest-song/greatest-song.component';
+import {Top10songComponent} from './components/songs/top10song/top10song.component';
+import {SongPlayerComponent} from './components/songs/song-player/song-player.component';
+import {SongDescriptionComponent} from './components/songs/song-description/song-description.component';
 import {SongEditComponent} from './components/songs/song-edit/song-edit.component';
-import { SongDeleteComponent } from './components/songs/song-delete/song-delete.component';
-import { AllPlaylistComponent } from './components/playlists/all-playlist/all-playlist.component';
-import { PlaylistDetailComponent } from './components/playlists/playlist-detail/playlist-detail.component';
+import {SongDeleteComponent} from './components/songs/song-delete/song-delete.component';
+import {AllPlaylistComponent} from './components/playlists/all-playlist/all-playlist.component';
+import {PlaylistDetailComponent} from './components/playlists/playlist-detail/playlist-detail.component';
 import {NgxAudioPlayerModule} from 'ngx-audio-player';
-import { SearchSongFormComponent } from './components/songs/search-song-form/search-song-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import {MatInputModule} from "@angular/material/input";
-import {CreateArtistComponent} from "./components/artists/create-artist/create-artist.component";
+import {SearchSongFormComponent} from './components/songs/search-song-form/search-song-form.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import {MatInputModule} from '@angular/material/input';
+import {CreateArtistComponent} from './components/artists/create-artist/create-artist.component';
 import {AddAuthorComponent} from './components/songs/create-song/add-author/add-author.component';
 import {AddSingerComponent} from './components/songs/create-song/add-singer/add-singer.component';
-import { UserPlaylistComponent } from './components/playlists/user-playlist/user-playlist.component';
+import {UserPlaylistComponent} from './components/playlists/user-playlist/user-playlist.component';
 import {NewPlaylistComponent} from './components/playlists/new-playlist/new-playlist.component';
-import { AddSongComponent } from './components/playlists/add-song/add-song.component';
-import { FindSongForPlaylistComponent } from './components/playlists/add-song/find-song-for-playlist/find-song-for-playlist.component';
+import {AddSongComponent} from './components/playlists/add-song/add-song.component';
+import {FindSongForPlaylistComponent} from './components/playlists/add-song/find-song-for-playlist/find-song-for-playlist.component';
 import {UserSongsAndPlaylistComponent} from './components/user/user-songs-and-playlist/user-songs-and-playlist.component';
 import {CommentComponent} from './components/user/comment/comment.component';
 import {UserProfileComponent} from './components/user/user-profile/user-profile.component';
-import { TopLikeSongComponent } from './components/songs/top-like-song/top-like-song.component';
-import { TopViewSongComponent } from './components/songs/top-view-song/top-view-song.component';
-import { TopLikePlaylistComponent } from './components/playlists/top-like-playlist/top-like-playlist.component';
-import { TopViewPlaylistComponent } from './components/playlists/top-view-playlist/top-view-playlist.component';
-import { TopTrendingComponent } from './components/tops/top-trending/top-trending.component';
-import {ToastrModule, ToastrService} from 'ngx-toastr';
-import {ISong} from './interfaces/isong';
-import {StorageService} from './services/storage.service';
+import {TopLikeSongComponent} from './components/songs/top-like-song/top-like-song.component';
+import {TopViewSongComponent} from './components/songs/top-view-song/top-view-song.component';
+import {TopLikePlaylistComponent} from './components/playlists/top-like-playlist/top-like-playlist.component';
+import {TopViewPlaylistComponent} from './components/playlists/top-view-playlist/top-view-playlist.component';
+import {TopTrendingComponent} from './components/tops/top-trending/top-trending.component';
+import {ToastrModule} from 'ngx-toastr';
 import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
-import { ChangePasswordComponent } from './components/user/change-password/change-password.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-export function getPostTimeToString(postTime):string {
-    let date = new Date(postTime);
-    let string = date.toDateString();
-    string = string.slice(4);
-    return string;
+import {ChangePasswordComponent} from './components/user/change-password/change-password.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+export function getPostTimeToString(postTime): string {
+  let date = new Date(postTime);
+  let string = date.toDateString();
+  string = string.slice(4);
+  return string;
 }
 
 
@@ -125,8 +124,8 @@ export function getPostTimeToString(postTime):string {
     CKEditorModule,
     NgbModule
   ],
-  providers: [authInterceptorProviders,{
-    provide: APP_BASE_HREF, useValue: "/"
+  providers: [authInterceptorProviders, {
+    provide: APP_BASE_HREF, useValue: '/'
   }],
   bootstrap: [AppComponent]
 })
