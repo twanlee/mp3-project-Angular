81b0d3d08286f37b0ed647f0675e2fa242e95474
twanlee <levantuan6898@gmail.com>
2020-08-31 18:22:00 +0700

    fix config firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBLhd86qXDoYUQ0fjzO8xMMmz0uMLakuM4',
+    authDomain: 'meomp3-5e362.firebaseapp.com',
+    databaseURL: 'https://meomp3-5e362.firebaseio.com',
+    projectId: 'meomp3-5e362',
+    storageBucket: 'meomp3-5e362.appspot.com',
+    messagingSenderId: '692199090966',
+    appId: '1:692199090966:web:2ad927a2f6fcb3047c7e37',
+    measurementId: 'G-NLYFC0QCGJ'
+  }
 };
