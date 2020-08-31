ec34357240e62d9b69df7f85a19dd12834b1badf
twanlee <levantuan6898@gmail.com>
2020-08-31 16:44:33 +0700

    con fix firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBLhd86qXDoYUQ0fjzO8xMMmz0uMLakuM4",
+    authDomain: "meomp3-5e362.firebaseapp.com",
+    databaseURL: "https://meomp3-5e362.firebaseio.com",
+    projectId: "meomp3-5e362",
+    storageBucket: "meomp3-5e362.appspot.com",
+    messagingSenderId: "G-NLYFC0QCGJ"
+  }
 };
